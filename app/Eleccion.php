4ea121d456c0f46ee0f0578a3daea514a57c0f26
eliasra07 <202001834@est.umss.edu>
2023-10-01 21:49:55 -0400

    funciona

--- app/Eleccion.php
+++ app/Eleccion.php
@@ -9,8 +9,10 @@ class Eleccion extends Model
     protected $table = 'elecciones'; // Nombre de la tabla en la base de datos
 
     protected $fillable = [
-        'nombre', 'motivo', 'cargodeautoridad', 'gestion', 'tipodevotantes', 'convocatoria', 'fecha', 'tipodeeleccion', 'descripcion'
+        'nombre', 'motivo', 'cargodeautoridad', 'gestion', 
+        'tipodevotantes', 'convocatoria', 'fecha', 'tipodeeleccion', 'descripcion'
     ];
+
 
     // Resto de las propiedades y métodos del modelo...
 }
--- app/Http/Controllers/EleccionController.php
+++ app/Http/Controllers/EleccionController.php
@@ -18,10 +18,21 @@ class EleccionController extends Controller
     {
         // Valida los datos del formulario según tus necesidades
 
+        /*$eleccion = new Eleccion();
+        $eleccion->nombre = $request->input('nombre');
+        $eleccion->candidato = $request->input('candidato');*/
         $eleccion = new Eleccion();
         $eleccion->nombre = $request->input('nombre');
-        $eleccion->candidato = $request->input('candidato');
-
+        $eleccion->motivo = $request->input('motivo');
+        $eleccion->cargodeautoridad = $request->input('cargo_autoridad');
+        $eleccion->gestion = $request->input('gestion');
+        $eleccion->tipodevotantes = $request->input('tipo_votantes');
+        
+        $eleccion->fecha = $request->input('fecha');
+        $eleccion->tipodeeleccion = $request->input('tipo_eleccion');
+        $eleccion->descripcion = $request->input('descripcion');
+        
+        
         // Guarda el archivo de convocatoria si se ha proporcionado
         if ($request->hasFile('convocatoria')) {
             $archivoConvocatoria = $request->file('convocatoria');
@@ -29,7 +40,7 @@ class EleccionController extends Controller
             $archivoConvocatoria->storeAs('convocatorias', $nombreArchivo, 'public');
             $eleccion->convocatoria = $nombreArchivo;
         }
-
+        //dd($eleccion->cargodeautoridad);
         $eleccion->save();
 
         return redirect()->route('layouts.registroeleccion')->with('success', 'Elección registrada exitosamente');
