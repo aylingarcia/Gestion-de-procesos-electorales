ec3cf8a9d363ae444833468241b2959da7886839
Sergio <servesa07@gmail.com>
2023-10-02 01:00:59 -0400

    Ahora si funciona como debe la DB

--- resources/views/elecciones/form.blade.php
+++ resources/views/elecciones/form.blade.php
@@ -1,30 +1,39 @@
-Crear una eleccion
-<label for="nombre">Nombre de la Elección:</label>
-    <input type="text" name="nombre" value="{{ $elecciones->nombre }}" required><br><br>
+Crear/Editar una elección
+<form action="{{ isset($elecciones) ? url('/elecciones/'.$elecciones->id) : url('/elecciones') }}" method="post" enctype="multipart/form-data">
+    @csrf
+    @if(isset($elecciones))
+        {{ method_field('PATCH') }}
+    @endif
+
+    <label for="nombre">Nombre de la Elección:</label>
+    <input type="text" name="nombre" value="{{ isset($elecciones) ? $elecciones->nombre : '' }}" required><br><br>
 
     <label for="motivo">Motivo de la Elección:</label>
-    <input type="text" name="motivo" value="{{ $elecciones->motivo }}" id="motivo" required><br><br>
+    <input type="text" name="motivo" value="{{ isset($elecciones) ? $elecciones->motivo : '' }}" id="motivo" required><br><br>
 
-    <label for="cargoadeutoridad">Cargo de Autoridad:</label>
-    <input type="text" name="cargodeautoridad" value="{{ $elecciones->cargodeautoridad }}" id="cargodeautoridad" required><br><br>
+    <label for="cargodeautoridad">Cargo de Autoridad:</label>
+    <input type="text" name="cargodeautoridad" value="{{ isset($elecciones) ? $elecciones->cargodeautoridad : '' }}" id="cargodeautoridad" required><br><br>
 
     <label for="gestion">Gestión (Año):</label>
-    <input type="number" name="gestion" value="{{ $elecciones->gestion }}" id="gestion" required><br><br>
+    <input type="number" name="gestion" value="{{ isset($elecciones) ? $elecciones->gestion : '' }}" id="gestion" required><br><br>
 
     <label for="tipodevotantes">Tipo de Votantes:</label>
-    <input type="text" name="tipodevotantes" value="{{ $elecciones->tipodevotantes }}" id="tipodevotantes" required><br><br>
+    <input type="text" name="tipodevotantes" value="{{ isset($elecciones) ? $elecciones->tipodevotantes : '' }}" id="tipodevotantes" required><br><br>
 
     <label for="convocatoria">Convocatoria (PDF):</label>
-    {{ $elecciones->convocatoria }}
-    <input type="file" name="convocatoria" value="" id="convocatoria" required><br><br>
+    @if(isset($elecciones) && $elecciones->convocatoria)
+        <p>{{ $elecciones->convocatoria }}</p>
+    @endif
+    <input type="file" name="convocatoria" {{ isset($elecciones) && $elecciones->convocatoria ? '' : 'required' }}><br><br>
 
     <label for="fecha">Fecha:</label>
-    <input type="date" name="fecha" value="{{ $elecciones->fecha }}" id="fecha" required><br><br>
+    <input type="date" name="fecha" value="{{ isset($elecciones) ? $elecciones->fecha : '' }}" id="fecha" required><br><br>
 
     <label for="tipodeeleccion">Tipo de Elección:</label>
-    <input type="text" name="tipodeeleccion" value="{{ $elecciones->tipodeeleccion }}" id="tipodeeleccion" required><br><br>
+    <input type="text" name="tipodeeleccion" value="{{ isset($elecciones) ? $elecciones->tipodeeleccion : '' }}" id="tipodeeleccion" required><br><br>
 
     <label for="descripcion">Descripción:</label>
-    <textarea name="descripcion" value="{{ $elecciones->descripcion }}" id="descripcion" rows="4"></textarea><br><br>
+    <textarea name="descripcion" id="descripcion" rows="4">{{ isset($elecciones) ? $elecciones->descripcion : '' }}</textarea><br><br>
 
-    <input type="submit" value="Guardar">
+    <input type="submit" value="{{ isset($elecciones) ? 'Actualizar' : 'Guardar' }}">
+</form>
