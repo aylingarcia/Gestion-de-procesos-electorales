cfabbba18036f753c3bd7ab10ca06450ddc3c703
aylingarcia <aylingarcianicol@gmail.com>
2023-10-07 18:06:57 -0400

    mensade de confirmacion de  cancelar y registrar

---
+++ resources/views/elecciones/index.blade.php
@@ -0,0 +1,30 @@
+mostrar lista de elecciones
+<table class="table table-light">
+
+    <thead class="thead-light">
+        <tr>
+            <th>Nombre de eleccion</th>
+            <th>Cargo de Autoridad</th>
+            <th>Gestion</th>
+
+            <th>Acciones</th>
+        </tr>
+    </thead>
+
+    <tbody>
+        @foreach( $eleccionescreadas as $elecciones)
+        <tr>
+            <td>{{ $elecciones->id }}</td>
+            <td>{{ $elecciones->nombre }}</td>
+            <td>{{ $elecciones->cargodeautoridad }}</td>
+            <td>{{ $elecciones->gestion }}</td>
+            <td>
+                
+            <a href="{{ url('/elecciones/'.$elecciones->id.'/edit') }}">
+                Editar 
+            </a> | Archivar</td>
+        </tr>
+        @endforeach
+    </tbody>
+
+</table>
