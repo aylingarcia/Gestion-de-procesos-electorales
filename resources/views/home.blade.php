5228f0c33be5313c23492bb1e0a9f237d963ee50
Enrique Delgadillo <202000658@est.umss.edu>
2023-10-12 19:28:25 -0400

    Mejoras en el Home, chau linea, colores

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -26,97 +26,85 @@
         }
 
         nav {
-            display: flex;
-            align-items: center;
-            justify-content: space-around;
-            height: 70px;
-            background-image: linear-gradient(to right, #003770, #f80211);
-            border-bottom: 2px solid #fff;
-            position: fixed;
-            top: 0;
-            left: 0;
-            width: 100%;
-            z-index: 999;
-        }
+    display:flex;
+    align-items: center;
+    justify-content: space-around;
+    height: 70px;
+    background-image: linear-gradient(to right, #003770, #f80211);
+    border-bottom: 2px solid #fff;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    z-index: 999;
+}
+nav .logo a {
+    font-size:25px;
+    color:#fff;
+    font-weight: 600;
+    text-decoration:none;
+}
+nav .logo a:hover {
+    color: #003770;
+    transition: 0.5s;
+}
+nav ul {
+    display: flex;
+    align-items: center;
+    justify-content:center;
+    gap:2.5rem;
+}
+nav ul li{
+    list-style: none;
+}
+nav ul li a{
+    color: #fff;
+    text-decoration: none;
+    font-size: 15px;
+    font-weight: 500;
+}
+nav ul li a:hover{
+    color: #003770;
+    transition: 0.5s;
+}
 
-        nav .logo a {
-            font-size: 25px;
-            color: #fff;
-            font-weight: 600;
-            text-decoration: none;
-        }
-
-        nav .logo a:hover {
-            color: #003770;
-            transition: 0.5s;
-        }
-
-        nav ul {
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            gap: 2.5rem;
-        }
-
-        nav ul li {
-            list-style: none;
-        }
-
-        nav ul li a {
-            color: #fff;
-            text-decoration: none;
-            font-size: 15px;
-            font-weight: 500;
-        }
-
-        nav ul li a:hover {
-            color: #003770;
-            transition: 0.5s;
-        }
-
-        .menu-icon {
-            display: none;
-            width: 25px;
-            height: 3px;
-            background: #fff;
-            transform: translateY(-50%);
-            transition: 0.5s;
-            border-radius: 5px;
-            cursor: pointer;
-        }
-
-        .menu-icon::before,
-        .menu-icon::after {
-            content: "";
-            position: absolute;
-            width: 25px;
-            height: 3px;
-            background: #fff;
-            transition: 0.5s;
-            border-radius: 5px;
-        }
-
-        .menu-icon::before {
-            top: -8px;
-        }
-
-        .menu-icon::after {
-            top: -8px;
-        }
-
-        .menu-icon.active {
-            background: rgba(0, 0, 0, 0);
-        }
-
-        .menu-icon.active::before {
-            top: 0;
-            transform: rotate(45deg);
-        }
-
-        .menu-icon.active::after {
-            top: 0;
-            transform: rotate(135deg);
-        }
+.menu-icon{
+    display: none;
+    width: 25px;
+    height:3px;
+    background: #fff;
+    transform: translateY(-50%);
+    transition: 0.5s;
+    border-radius:5px;
+    cursor: pointer;
+}
+.menu-icon::before,
+.menu-icon::after{
+    content:"";
+    position: absolute;
+    width: 25px;
+    height:3px;
+    background: #fff;
+    transition:0.5s;
+    border-radius: 5px;
+}
+.menu-icon::before{
+    top:-8px;
+}
+.menu-icon::after{
+    top:-8px;
+}
+.menu-icon.active{
+    background: rgba(0,0,0,0);
+}
+.menu-icon.active::before{
+    top:0;
+    transform:rotate(45deg);
+}
+.menu-icon.active::after{
+    top:0;
+    transform: rotate(135deg);
+}
 
         @media screen and (max-width: 992px) {
             nav ul {
@@ -160,30 +148,6 @@
 
 
 
-        .header {
-            background-image: linear-gradient(to right, #003770, #C20000);
-            color: white;
-            text-align: center;
-            padding: 40px 0;
-            background-size: cover;
-            background-repeat: no-repeat;
-            background-attachment: fixed;
-        }
-
-        .header h1 {
-            font-size: 36px;
-            font-weight: bold;
-        }
-
-        .header p {
-            font-size: 18px;
-            font-weight: 300;
-        }
-
-        .votante-form-container {
-            padding: 20px;
-            margin-top: 100px;
-        }
 
         .footer {
             background-color: #003770;
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,17 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
     <style>
-        .company-logo {
-    border-radius: 8%;
+ 
 
-    max-width: 21%;
-    /* Ajusta el ancho máximo de la imagen al 100% del contenedor */
-    height: auto;
-    /* Permite que la altura se ajuste automáticamente para mantener la proporción */
-    /* Alinea verticalmente la imagen en el medio del texto */
-    float: left;
-    margin-right: 40px;
-}
         * {
             margin: 0;
             padding: 0;
@@ -32,91 +23,90 @@
     /* Alinea verticalmente la imagen en el medio del texto */
     float: left;
     margin-right: 40px;
+
 }
-        nav {
-            display: flex;
-            align-items: center;
-            justify-content: space-around;
-            height: 70px;
-            background-color: #003770;
-            /*background-color: #C20000;*/
-            /*background-image: linear-gradient(to right, #003770, #C20000);*/    
+nav {
 
-           
-            border-bottom: 2px solid #fff;
-            position: fixed;
-            top: 0;
-            left: 0;
-            width: 100%;
-            z-index: 999;
-        }
-        nav .logo a {
-            font-size: 25px;
-            color: #fff;
-            font-weight: 600;
-            text-decoration: none;
-        }
-        nav .logo a:hover {
-            color: #003770;
-            transition: 0.5s;
-        }
-        nav ul {
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            gap: 2.5rem;
-        }
-        nav ul li {
-            list-style: none;
-        }
-        nav ul li a {
-            color: #fff;
-            text-decoration: none;
-            font-size: 15px;
-            font-weight: 500;
-        }
-        nav ul li a:hover {
-            color: #003770;
-            transition: 0.5s;
-        }
+    display:flex;
+    align-items: center;
+    justify-content: space-around;
+    height: 70px;
+    background-image: linear-gradient(to right, #003770, #f80211);
+    /* Linea que separa el header con el body */
+    /* border-bottom: 1px solid #ffffff; */
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    z-index: 999;
+}
+nav .logo a {
+    font-size:25px;
+    color:#fff;
+    font-weight: 600;
+    text-decoration:none;
+}
+nav .logo a:hover {
+    color: #003770;
+    transition: 0.5s;
+}
+nav ul {
+    display: flex;
+    align-items: center;
+    justify-content:center;
+    gap:2.5rem;
+}
+nav ul li{
+    list-style: none;
+}
+nav ul li a{
+    color: #fff;
+    text-decoration: none;
+    font-size: 15px;
+    font-weight: 500;
+}
+nav ul li a:hover{
+    color: #003770;
+    transition: 0.5s;
+}
 
-        .menu-icon {
-            display: none;
-            width: 25px;
-            height: 3px;
-            background: #fff;
-            transform: translateY(-50%);
-            transition: 0.5s;
-            border-radius: 5px;
-            cursor: pointer;
-        }
-        .menu-icon::before,
-        .menu-icon::after {
-            content: "";
-            position: absolute;
-            width: 25px;
-            height: 3px;
-            background: #fff;
-            transition: 0.5s;
-            border-radius: 5px;
-        }
-        .menu-icon::before {
-            top: -8px;
-        }
-        .menu-icon::after {
-            top: -8px;
-        }
-        .menu-icon.active {
-            background: rgba(0, 0, 0, 0);
-        }
-        .menu-icon.active::before {
-            top: 0;
-            transform: rotate(45deg);
-        }
-        .menu-icon.active::after {
-            top: 0;
-            transform: rotate(135deg);
-        }
+.menu-icon{
+    display: none;
+    width: 25px;
+    height:3px;
+    background: #fff;
+    transform: translateY(-50%);
+    transition: 0.5s;
+    border-radius:5px;
+    cursor: pointer;
+}
+.menu-icon::before,
+.menu-icon::after{
+    content:"";
+    position: absolute;
+    width: 25px;
+    height:3px;
+    background: #fff;
+    transition:0.5s;
+    border-radius: 5px;
+}
+.menu-icon::before{
+    top:-8px;
+}
+.menu-icon::after{
+    top:-8px;
+}
+.menu-icon.active{
+    background: rgba(0,0,0,0);
+}
+.menu-icon.active::before{
+    top:0;
+    transform:rotate(45deg);
+}
+.menu-icon.active::after{
+    top:0;
+    transform: rotate(135deg);
+}
         @media screen and (max-width: 992px) {
             nav ul {
                 position: fixed;
@@ -141,49 +131,21 @@
         }
 
         
-        body {
-            
+        .fondoooo {
+            margin: 0;
+                padding: 0;
                 font-family: Arial, sans-serif;
-                background-image: linear-gradient(to right, #003770, #C20000);
+                background-image: linear-gradient(to right, #003770, #f80211);
                 /*background: repeating-linear-gradient(45deg, transparent, transparent 10px, rgba(0, 55, 112, 0.4) 10px, rgba(0, 55, 112, 0.4) 20px);*/
                 /*background: linear-gradient(to bottom, #003770, #C20000);*/
                 /*background: repeating-linear-gradient(60deg, #003770 0px, #003770 10px, #C20000 10px, #C20000 20px);*/    
                 /*background: radial-gradient(circle at 50% 50%, #C20000,#003770), repeating-radial-gradient(circle, transparent 0, transparent 5px, #333 5px, #333 10px);*/
-                
-                margin: 0;
-                padding: 0;
+
                 color: white;
-                position: relative;
-                overflow: hidden;
             }
 
          
 
-
-        .header {
-            background-image: linear-gradient(to right, #003770, #C20000);
-            color: white;
-            text-align: center;
-            padding: 40px 0;
-            background-size: cover;
-            background-repeat: no-repeat;
-            background-attachment: fixed;
-        }
-
-        .header h1 {
-            font-size: 36px;
-            font-weight: bold;
-        }
-
-        .header p {
-            font-size: 18px;
-            font-weight: 300;
-        }
-
-        .votante-form-container {
-            padding: 20px;
-            margin-top: 100px; 
-        }
 
         .footer {
             background-color: #003770;
@@ -227,6 +189,7 @@
             padding-left: 20px;
         }
     </style>
+    
 </head>
 
 <body>
@@ -250,13 +213,15 @@
         <div class="menu-icon"></div>
     </nav>
     <!-- Header personalizado -->
+
+
+    <section class="fondoooo">
     <div class="content-container">
         <div class="title">
             <h1>Tribunal Electoral Universitario</h1>
             <p>El TEU es responsable de las elecciones democráticas dentro de la Universidad Mayor de San Simón.</p>
         </div>
     </div>
-
     <div class="footer">
     <span>
             Av. Oquendo y calle Jordán 
@@ -287,5 +252,5 @@
             Design: DevGenius </span>
     </div>
 </body>
-
+</section>
 </html>
